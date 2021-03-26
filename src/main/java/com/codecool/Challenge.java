75eb3d026010730ba6d010a5a27920ca346b0289
Zoe Anna <horseprime5@gmail.com>
2021-03-26 11:32:39 +0100

    feladat1

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -4,5 +4,24 @@ public class Challenge {
 
     public static int minPositive(int a, int b) {
 
+        if (a > 0 || b > 0) {
+
+            if (a > 0 && b > 0 ) {
+                if (a < b) {
+                    return a;
+                } else {
+                    return b;
+                }
+            } else if(b < 0) {
+                return a;
+            } else {
+                return b;
+            }
+
+        } else {
+            return -1;
+        }
+
     }
 }
+
